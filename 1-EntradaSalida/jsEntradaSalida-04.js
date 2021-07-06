d8cb5cc65965b579ed095301be5cf69ed3fc9549
emanuel david acuña <emaacua@gmail.com>
2021-07-06 18:54:32 -0300

    primeros 5 ejercicios resueltos

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,31 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombre;//reservo espacio para guardar el nombre del usuario
+	nombre = prompt ("Ingrese su nombre");//guardo la variable nombre, el tecto que escribio el usuario dentro de la ventana prompt
+	document.getElementById("tctIdNombre").value = nombre;//copio el nombre que tengo en la variable de nombre edentro de la ventana
+
 }
+/*function mostrar()
+{	
+	var nombre;
+	var mensaje;
+	nombre = prompt ("Ingrese su nombre", "Emanuel");
+	mensaje ="su nombre es" + nombre;
+	document.getElementById("txtIdNombre").Value=nmensaje;
+
+}
+
+function mostrar()
+{	
+	var nombre;
+	var apellido;
+	var mensaje;
+	nombre = prompt ("Ingrese su nombre", "Emanuel");
+	apellido = prompt ("Ingrese su apellido");
+	mensaje ="su nombre es" + nombre + "y su apellido es" + apellido;
+	document.getElementById("txtIdNombre").Value=nmensaje;
+
+}
+*/
 
