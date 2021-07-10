f253ebd6f7d9b80b30ad9337ff3be1b591f1c65f
emanuel david acuña <emaacua@gmail.com>
2021-07-09 22:57:33 -0300

    tp2 resuelto

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,62 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+var largo;
+var ancho;
+var calculo1;
+var calculo2;
+
+
+largo = document.getElementById("txtIdLargo").value;
+laego = parseInt(largo);
+
+ancho = document.getElementById("txtIdAncho").value;
+ancho = parseInt(ancho);
+
+calculo1 = largo * ancho;
+
+calculo2 = calculo1 * 3;
+
+alert("la cantidad de alambre a usar es: " + calculo2 +   " Metros de alambre");
+
 
 }
 function Circulo () 
 {
-	
+	var radio;
+    var calculo1;
+    var calculo2;
+    var total;
+
+    radio = document.getElementById("txtIdRadio" ).value;
+    radio = parseInt(radio);
+
+    calculo1 = radio * 2;
+    calculo2 = 3.14 * calculo1;
+    total = calculo2 * 3;
+
+    alert("el total de alambre a comprar es: " + total +  " mts");
+
+
 }
 function Materiales () 
 {
-	
+    var largo;
+    var ancho;
+    var superficie;
+    var cemento;
+    var cal;
+
+    largo = document.getElementById("txtIdLargo").value;
+    laego = parseInt(largo);
+
+    ancho = document.getElementById("txtIdAncho").value;
+    ancho = parseInt(ancho);
+
+    superficie = largo * ancho;
+    cemento = superficie * 2;
+    cal = superficie * 3;
+
+    alert("para un contrapiso de: " + superficie + " metros cuadrados se necesitan " + cemento + " bolsas de cemento y " + cal + " bolsas de cal");
+
 }
